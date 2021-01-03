a68d4f2aaabd6625ef14484dbc962418837d4f5b
Anton Sarychev <dvidja@gmail.com>
2021-01-03 12:37:54 +0300

    test init
    test init

---
+++ src/cryptopg.cpp
@@ -0,0 +1,8 @@
+#include "../include/cryptopglib/cryptopg.h"
+
+#include <iostream>
+
+void PrintHelloWorld()
+{
+    std::cout << "Hello world" << std::endl;
+}
---
+++ test/main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Anton Sarychev on 1/3/21.
+//
+#include "cryptopglib/cryptopg.h"
+
+int main()
+{
+    PrintHelloWorld();
+}
