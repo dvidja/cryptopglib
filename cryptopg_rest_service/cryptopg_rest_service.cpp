51ed1f4de39909a8b5c85633742fd7d08cc6b08c
Anton Sarychau <dvidja@gmail.com>
2023-11-17 17:11:10 +0300

    add cryptopg_rest_service.cpp with cpprestsdk to make tests with rest service

---
+++ cryptopg_rest_service/cryptopg_rest_service.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Anton Sarychev on 16.11.23.
+//
+
+#include <iostream>
+
+#include <cpprest/http_client.h>
+
+int main() {
+    web::http::client::http_client client("http://google.com");
+    web::http::http_request req(web::http::methods::GET);
+
+    auto resp = client.request(req).get();
+    std::cout << resp.status_code() << " : sync request" << std::endl;
+    std::cout << resp.extract_string(true).get() << std::endl;
+    std::cout << "Starting service...." << std::endl;
+}
